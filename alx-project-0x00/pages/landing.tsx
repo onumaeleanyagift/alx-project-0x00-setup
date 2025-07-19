53d3bda098e9a7c199f9f85a9711227d989390a3
onumaeleanyagift <onumaeleanyagift@gmail.com>
2025-07-19 17:01:52 +0100

    feat: render Card component on Landing page

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,7 +1,13 @@
+import Card from "@/components/Card";
+
 const Landing: React.FC = () => {
   return (
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 };
