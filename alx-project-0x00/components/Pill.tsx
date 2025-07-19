8b7b24d9dad88176d02ac4cd4f6f4c08fa98e5ef
onumaeleanyagift <onumaeleanyagift@gmail.com>
2025-07-19 17:08:48 +0100

    refactor: update Pill component to accept title prop using PillProps interface

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,9 +1,6 @@
-interface PillProps {
-  title: string;
-}
+import { PillProps } from "@/interfaces";
 
 const Pill: React.FC<PillProps> = ({ title }) => {
-
   return (
     <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
       <p className=" text-sm ">Title</p>
